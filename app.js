415724239197fc4b741ab85f942779e7a36d5c9e
AjithGeorge94 <41148641+AjithGeorge94@users.noreply.github.com>
2018-10-12 14:56:03 +0530

    SKeletal Controllers

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var catalogRouter = require('./routes/catalog');  //Import routes for "catalog" area of site
 
 var app = express();
 
@@ -30,6 +31,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/catalog', catalogRouter);  // Add catalog routes to middleware chain.
 
 //Routes
 app.use('/', indexRouter);
